1358fc0da2334bf42f727de151b60896d4db4ba2
menjaw <menjaholm@gmail.com>
2017-08-22 15:33:53 +0200

    insert items with sql script and JPA finish

--- JpaBasic_FirstDay/src/main/resources/scripts/Data.sql
+++ JpaBasic_FirstDay/src/main/resources/scripts/Data.sql
@@ -7,9 +7,17 @@
  * Author:  Menja
  * Created: 21-08-2017
  */
-drop database jpabasic_firstday;
-create database jpabasic_firstday;
+USE jpabasic_firstday;
 
-
-use jpabasic_firstday
+TRUNCATE TABLE book;
 INSERT INTO book (ID, PRICE, TITLE) VALUES (0, 456.55 , "FoodStuff");
+INSERT INTO book (ID, PRICE, TITLE) VALUES (0, 722.55 , "Coding");
+INSERT INTO book (ID, PRICE, TITLE) VALUES (0, 96.55 , "Training");
+INSERT INTO book (ID, PRICE, TITLE) VALUES (0, 712.55 , "Animals");
+INSERT INTO book (ID, PRICE, TITLE) VALUES (0, 24.55 , "Nature");
+INSERT INTO book (ID, PRICE, TITLE) VALUES (0, 34.55 , "Nike");
+INSERT INTO book (ID, PRICE, TITLE) VALUES (0, 52.55 , "Casall");
+INSERT INTO book (ID, PRICE, TITLE) VALUES (0, 71.55 , "Biger Mikkelsen");
+INSERT INTO book (ID, PRICE, TITLE) VALUES (0, 29.55 , "Lenovo computers");
+INSERT INTO book (ID, PRICE, TITLE) VALUES (0, 139.55 , "HP computers");
+INSERT INTO book (ID, PRICE, TITLE) VALUES (0, 820.55 , "Audi TT");
