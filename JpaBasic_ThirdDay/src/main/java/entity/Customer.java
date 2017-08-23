fbdf899e102ca8a14c90f979939ddacb60757368
menjaw <menjaholm@gmail.com>
2017-08-23 11:39:28 +0200

    Exercise - JPA Entity Mapping 3 done

---
+++ JpaBasic_ThirdDay/src/main/java/entity/Customer.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Menja
+ */
+@Entity
+public class Customer implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public static double getPrice(double priceItem, int quantity) {
+        return priceItem * quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Customer[ id=" + id + " ]";
+    }
+
+}
---
+++ JpaBasic_ThirdDay/src/main/java/entity/DiscountFixed.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Menja
+ */
+@Entity
+public class DiscountFixed extends DiscountType implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    double discount = 0.1;
+    @Override
+    public double calcDiscount(double priceItem, int quantity){
+    return priceItem * discount * quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.DiscountFixed[ id=" + id + " ]";
+    }
+
+}
